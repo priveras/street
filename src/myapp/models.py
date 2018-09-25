888fcd459fb91774be7d34bd1259f8d68974009b
Pablo Rivera <priveras@gmail.com>
2018-09-25 10:34:27 -0400

    info to assumptions

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -80,7 +80,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
 
     def __str__(self):
-        return str(self.project)
+        return str(self.user)
 
 class Link(models.Model):
     project = models.ForeignKey(Project)
