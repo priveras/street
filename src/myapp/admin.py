e3b53d7a059a00c6199eb9f6169bbd10c6a335a0
andreaslarsen13 <andreas.larsen@zx-ventures.com>
2018-09-26 15:49:29 -0400

    Add Zone Model

--- src/myapp/admin.py
+++ src/myapp/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Project, Team, Comment, Assumption, Problem, BusinessModel, Metric, File, Past, Future, Summary, Solution, Tutorial, Profile, Link, Dvf, Progress
+from .models import Project, Team, Comment, Assumption, Problem, BusinessModel, Metric, File, Past, Future, Summary, Solution, Tutorial, Profile, Link, Dvf, Progress, Zone
 
 class ProjectAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
@@ -21,6 +21,7 @@ admin.site.register(Assumption)
 admin.site.register(Problem)
 admin.site.register(BusinessModel)
 admin.site.register(Solution)
+admin.site.register(Zone)
 admin.site.register(Metric)
 admin.site.register(File)
 admin.site.register(Past)
