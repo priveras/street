284a8cfa61500e1892f68cfecad6488848434d6e
Pablo Rivera <priveras@gmail.com>
2018-09-26 13:06:51 -0400

    progress

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -138,7 +138,7 @@ class HomeView(generic.ListView):
 
         context['projects_list'] = Project.objects.filter(team__user=self.request.user)
         context['alert'] = alert
-        context['permission'] = Team.objects.filter(project=self.object).filter(user=self.request.user).filter(permission="edit")
+        #context['permission'] = Team.objects.filter(project=self.object).filter(user=self.request.user).filter(permission="edit")
 
         return context
 
