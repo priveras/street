03764838a9ca1c3ee1752a78351ee454e1af4e1c
Pablo Rivera <priveras@gmail.com>
2018-10-12 16:06:22 -0400

    nots

--- src/mydjango/settings.py
+++ src/mydjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-au3&glnt&h(g^!s$rjjgh6szj=$(wxnelw2uiw+6^83+5ab^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
